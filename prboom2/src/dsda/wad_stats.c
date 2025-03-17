42e32fdbe1ea6af617ec0754a3ef9624b51c8cc4
Arsinikk <me@andrikpowell.com>
2025-03-17 14:00:59 -0500

    Revert "Remap "5 -> num_skills" and "4 -> num_skills - 1""
    This reverts commit 6a889e1b08c2b4e4af3de327fc0d81804a956a89.

--- prboom2/src/dsda/wad_stats.c
+++ prboom2/src/dsda/wad_stats.c
@@ -275,7 +275,7 @@ void dsda_WadStatsExitMap(int missed_monsters) {
   if (!nomonsters) {
     skill = gameskill + 1;
     if (skill > current_map_stats->best_skill) {
-      if (current_map_stats->best_skill < num_skills - 1) {
+      if (current_map_stats->best_skill < 4) {
         current_map_stats->best_time = -1;
         current_map_stats->best_max_time = -1;
       }
@@ -283,12 +283,12 @@ void dsda_WadStatsExitMap(int missed_monsters) {
       current_map_stats->best_skill = skill;
     }
 
-    if (skill >= current_map_stats->best_skill || skill == num_skills - 1) {
+    if (skill >= current_map_stats->best_skill || skill == 4) {
       if (levels_completed == 1)
         if (current_map_stats->best_time == -1 || current_map_stats->best_time > leveltime)
           current_map_stats->best_time = leveltime;
 
-      if (levels_completed == 1 && skill == num_skills)
+      if (levels_completed == 1 && skill == 5)
         if (current_map_stats->best_nm_time == -1 || current_map_stats->best_nm_time > leveltime)
           current_map_stats->best_nm_time = leveltime;
 
