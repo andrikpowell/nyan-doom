c732924a81099f9015492ec241ec1f5e74538943
Arsinikk <me@andrikpowell.com>
2025-04-27 22:06:48 -0500

    Set "skip iwad map names" to on by default

--- prboom2/src/dsda/configuration.c
+++ prboom2/src/dsda/configuration.c
@@ -488,7 +488,7 @@ dsda_config_t dsda_config[dsda_config_count] = {
   },
   [nyan_config_ignore_default_map_names] = {
    "nyan_ignore_default_map_names", nyan_config_ignore_default_map_names,
-   CONF_BOOL(0), NULL, STRICT_INT(0)
+   CONF_BOOL(1), NULL, STRICT_INT(0)
   },
   [nyan_config_play_random_music] = {
     "nyan_play_random_music", nyan_config_play_random_music,
