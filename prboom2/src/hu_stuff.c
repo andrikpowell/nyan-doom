785bb9b755fc7e21d05575eb7bdfb00b757847ac
Arsinikk <me@andrikpowell.com>
2025-05-26 18:06:02 -0500

    Add Automap Title / Author Cycle

--- prboom2/src/hu_stuff.c
+++ prboom2/src/hu_stuff.c
@@ -99,6 +99,7 @@ void HU_InitThresholds(void)
 }
 
 dsda_string_t hud_title;
+dsda_string_t hud_title_cycle;
 
 static void HU_FetchTitle(void)
 {
@@ -106,6 +107,19 @@ static void HU_FetchTitle(void)
     dsda_FreeString(&hud_title);
 
   dsda_HUTitle(&hud_title);
+}
+
+dsda_string_t hud_author;
+
+static void HU_FetchAuthor(void)
+{
+  const char* author = dsda_MapAuthor();
+
+  if (hud_author.string)
+    dsda_FreeString(&hud_author);
+
+  if (author && author[0])
+    dsda_StringPrintF(&hud_author, author);
 }
 
 static void HU_InitMessages(void)
@@ -319,17 +333,15 @@ void HU_AnnounceMap(void)
     if (gamemap != last_gamemap || gameepisode != last_gameepisode)
     {
       int announce_config = dsda_IntConfig(dsda_config_announce_map);
-      const char *author;
 
       last_gamemap = gamemap;
       last_gameepisode = gameepisode;
 
-      author = dsda_MapAuthor();
-      if (author && author[0])
+      if (hud_author.string)
       {
         dsda_string_t map_and_author;
 
-        dsda_StringPrintF(&map_and_author, "%s by %s", hud_title.string, author);
+        dsda_StringPrintF(&map_and_author, "%s by %s", hud_title.string, hud_author.string);
 
         if (announce_config==2)
           dsda_AddAlert(map_and_author.string);
@@ -365,6 +377,7 @@ void HU_Start(void)
   HU_InitPlayer();
   HU_InitMessages();
   HU_FetchTitle();
+  HU_FetchAuthor();
   HU_InitCrosshair();
 
   dsda_InitExHud();
@@ -476,6 +489,10 @@ void HU_Ticker(void)
       S_StartVoidSound(title_message_p->sfx);
     }
   }
+
+  // Reset map/author cycle when leaving automap
+  if (hud_title_cycle.string && !automap_active)
+    dsda_FreeString(&hud_title_cycle);
 
   dsda_UpdateExHud();
 }
