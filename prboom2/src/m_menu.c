fb0fef3f655c5770d5cbfe6aa5e84c269deeb10d
Arsinikk <me@andrikpowell.com>
2025-06-06 08:08:18 -0500

    Tweak Shareware Episode Behaviour

--- prboom2/src/m_menu.c
+++ prboom2/src/m_menu.c
@@ -649,18 +649,12 @@ static void M_DrawEpisode(void)
 
 void M_Episode(int choice)
 {
-  // Heretic shareware is different than Doom shareware in that it shows
-  // the episode select, but will display a message when selecting
-  // other episodes. This code shows that message and avoids a crash.
-  //
-  if (heretic && gamemode == shareware && choice && episodes[choice].vanilla) {
-    M_StartMessage(HERETIC_SWSTRING, NULL, false); // Ty 03/27/98 - externalized
-    M_SetupNextMenu(&ReadDef1);
-    return;
-  }
+  // Shareware now shows the episode select, but 
+  // displays message when selecting other episodes.
+  const char* shareware_msg = heretic ? HERETIC_SWSTRING : s_SWSTRING;
 
-  if (gamemode == shareware && choice && !episodes[choice].vanilla) {
-    M_StartMessage(s_SWSTRING, NULL, false); // Ty 03/27/98 - externalized
+  if (gamemode == shareware && choice && episodes[choice].vanilla) {
+    M_StartMessage(shareware_msg, NULL, false); // Ty 03/27/98 - externalized
     M_SetupNextMenu(&ReadDef1);
     return;
   }
