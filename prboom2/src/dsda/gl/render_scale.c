850b65dc520843af7c36e8b2af680eb9bb9c2e88
Arsinikk <me@andrikpowell.com>
2026-02-01 16:27:38 -0600

    Fix OpenGL bleeding line

--- prboom2/src/dsda/gl/render_scale.c
+++ prboom2/src/dsda/gl/render_scale.c
@@ -42,12 +42,11 @@ static int gl_clear_box_height;
 
 void dsda_GLSetRenderViewportParams() {
   extern SDL_Rect viewport_rect;
-  float gl_stbar_window_fix = !desired_fullscreen; // window black line fix - TODO: Come up with a better fix
   gl_scale_x = (float)viewport_rect.w / (float)SCREENWIDTH;
   gl_scale_y = (float)viewport_rect.h / (float)SCREENHEIGHT;
 
   // elim - This will be zero if no statusbar is being drawn
-  gl_statusbar_height = (int)(gl_scale_y * (float)ST_SCALED_HEIGHT + gl_stbar_window_fix) * R_PartialView();
+  gl_statusbar_height = (int)(gl_scale_y * (float)ST_SCALED_HEIGHT) * R_PartialView();
 
   gl_scene_offset_x = (int)(viewwindowx * gl_scale_x);
   gl_scene_offset_y = (int)(viewwindowy * gl_scale_y);
