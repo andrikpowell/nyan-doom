48fd6164e1ed3df479a605709f38379a319bc5e6
Arsinikk <me@andrikpowell.com>
2026-02-07 18:33:57 -0600

    Add "swirling flats" as Nyan option

--- prboom2/src/m_menu.c
+++ prboom2/src/m_menu.c
@@ -4477,7 +4477,7 @@ setup_menu_t display_options_settings[] = {
   { "Linear Sky Scrolling", S_YESNO, m_conf, G_X, dsda_config_render_linearsky, DEPEND_SW },
   { "Quake Intensity", S_PERC, m_conf, G_X, dsda_config_quake_intensity },
   { "Fake Contrast", S_CHOICE, m_conf, G_X, dsda_config_fake_contrast_mode, 0, fake_contrast_list },
-  { "Swirling Flats", S_CHOICE, m_conf, G_X, dsda_config_swirling_flats, 0, swirling_flat_list },
+  { "Swirling Flats", S_CHOICE | S_NYAN, m_conf, G_X, dsda_config_swirling_flats, 0, swirling_flat_list },
   EMPTY_LINE,
   { "GL Light Fade", S_CHOICE, m_conf, G_X, dsda_config_gl_fade_mode, 0, gl_fade_mode_list, DEPEND(dsda_config_videomode, OPENGL_MODE) },
   { "GL Health Bars", S_YESNO, m_conf, G_X, dsda_config_gl_health_bar, DEPEND_GL },
