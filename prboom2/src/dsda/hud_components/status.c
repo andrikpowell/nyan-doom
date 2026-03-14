86bc0880563dabc6a02df2be409895afac8deba3
Arsinikk <me@andrikpowell.com>
2026-03-13 23:47:42 -0500

    Allow status widget to draw in reverse - "left to right" or "bottom to top"

--- prboom2/src/dsda/hud_components/status.c
+++ prboom2/src/dsda/hud_components/status.c
@@ -28,6 +28,7 @@
 typedef struct {
     dsda_patch_component_t component;
     dboolean vertical;
+    dboolean reverse;
 } local_component_t;
 
 static local_component_t* local;
@@ -62,18 +63,25 @@ void drawPowerupStatusIcon(player_t* player, int* x, int* y, int powerup, const 
     if ((!blinking && powerup) || (blinking && (powerup > BLINKTHRESHOLD || (powerup & 8))))
         V_DrawNamePatch(*x, *y, lumpname, color, flags);
 
-    x += patch_spacing;
-    y += patch_vertical_spacing;
-
     if (local->vertical)
-        *y += PATCH_SIZE;
+        *y += local->reverse ? -(patch_vertical_spacing + PATCH_SIZE)   : (patch_vertical_spacing + PATCH_SIZE);
     else
-        *x += PATCH_SIZE;
+        *x += local->reverse ? -(patch_spacing + PATCH_SIZE)            : (patch_spacing + PATCH_SIZE);
 }
+
+typedef struct {
+  int enabled;
+  int powerup;
+  const char *lump;
+  int color;
+  int blinking;
+} status_icon_t;
 
 static void dsda_DrawComponent(void) {
     player_t* player;
     int x, y;
+    status_icon_t icons[8];
+    int n = 0;
 
     player = &players[displayplayer];
 
@@ -83,29 +91,42 @@ static void dsda_DrawComponent(void) {
     if(raven || !dsda_IntConfig(nyan_config_ex_status_widget))
         return;
 
-    if (player->armortype > 0 && dsda_IntConfig(nyan_config_ex_status_armor))
-        drawPowerupStatusIcon(player, &x, &y, player->armortype > 0, drawArmorName(player), CR_DEFAULT, NOT_BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_armor) && player->armortype > 0,
+                                  player->armortype > 0, drawArmorName(player), CR_DEFAULT, NOT_BLINKING };
 
-    if (player->powers[pw_strength] && dsda_IntConfig(nyan_config_ex_status_berserk))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_strength], "STFPPSTR", getBerserkColor(player), NOT_BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_berserk) && player->powers[pw_strength],
+                                  player->powers[pw_strength], "STFPPSTR", getBerserkColor(player), NOT_BLINKING };
 
-    if (player->powers[pw_allmap] && dsda_IntConfig(nyan_config_ex_status_areamap))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_allmap], "STFPMAP", CR_DEFAULT, NOT_BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_areamap) && player->powers[pw_allmap],
+                                  player->powers[pw_allmap], "STFPMAP", CR_DEFAULT, NOT_BLINKING };
 
-    if (player->backpack && dsda_IntConfig(nyan_config_ex_status_backpack))
-        drawPowerupStatusIcon(player, &x, &y, player->backpack, "STFPBPAK", CR_DEFAULT, NOT_BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_backpack) && player->backpack,
+                                  player->backpack, "STFPBPAK", CR_DEFAULT, NOT_BLINKING };
 
-    if (player->powers[pw_ironfeet] && dsda_IntConfig(nyan_config_ex_status_radsuit))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_ironfeet], "STFPSUIT", CR_DEFAULT, BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_radsuit) && player->powers[pw_ironfeet],
+                                  player->powers[pw_ironfeet], "STFPSUIT", CR_DEFAULT, BLINKING };
 
-    if (player->powers[pw_invisibility] && dsda_IntConfig(nyan_config_ex_status_invis))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_invisibility], "STFPINS", CR_DEFAULT, BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_invis) && player->powers[pw_invisibility],
+                                  player->powers[pw_invisibility], "STFPINS", CR_DEFAULT, BLINKING };
 
-    if (player->powers[pw_infrared] && dsda_IntConfig(nyan_config_ex_status_liteamp))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_infrared], "STFPVIS", CR_DEFAULT, BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_liteamp) && player->powers[pw_infrared],
+                                  player->powers[pw_infrared], "STFPVIS", CR_DEFAULT, BLINKING };
 
-    if (player->powers[pw_invulnerability] && dsda_IntConfig(nyan_config_ex_status_invuln))
-        drawPowerupStatusIcon(player, &x, &y, player->powers[pw_invulnerability], "STFPINV", CR_DEFAULT, BLINKING);
+    icons[n++] = (status_icon_t){ dsda_IntConfig(nyan_config_ex_status_invuln) && player->powers[pw_invulnerability],
+                                  player->powers[pw_invulnerability], "STFPINV", CR_DEFAULT, BLINKING };
+
+  if (local->reverse)
+  {
+    for (int i = n - 1; i >= 0; --i)
+      if (icons[i].enabled)
+        drawPowerupStatusIcon(player, &x, &y, icons[i].powerup, icons[i].lump, icons[i].color, icons[i].blinking);
+  }
+  else
+  {
+    for (int i = 0; i < n; ++i)
+      if (icons[i].enabled)
+        drawPowerupStatusIcon(player, &x, &y, icons[i].powerup, icons[i].lump, icons[i].color, icons[i].blinking);
+  }
 }
 
 void dsda_InitStatusHC(int x_offset, int y_offset, int vpt, int* args, int arg_count, void** data) {
@@ -113,6 +134,7 @@ void dsda_InitStatusHC(int x_offset, int y_offset, int vpt, int* args, int arg_c
     local = *data;
 
     local->vertical = arg_count > 0 ? !!args[0] : false;
+    local->reverse  = arg_count > 1 ? !!args[1] : false;
 
     dsda_InitPatchHC(&local->component, x_offset, y_offset, vpt);
 }
