652f10d6439ff86439ff9ec9b68f145efbdaf599
CoderPenguin1-dev <coderpenguin1.dev@gmail.com>
2026-07-06 05:48:18 -0400

    Overhaul label drawing and fix text color

--- prboom2/src/dsda/hud_components/map_time.c
+++ prboom2/src/dsda/hud_components/map_time.c
@@ -20,12 +20,42 @@
 #include "map_time.h"
 
 typedef struct {
+  dsda_text_t label;
   dsda_text_t component;
   dboolean always_show_both;
   dboolean show_labels;
 } local_component_t;
 
 static local_component_t* local;
+
+static void dsda_UpdateLabelComponentText(char* str, size_t max_size) {
+  size_t length = 0;
+  int level_time;
+  int total_time;
+
+  total_time = hexen ?
+               players[consoleplayer].worldTimer :
+               totalleveltimes + leveltime;
+  level_time = leveltime;
+
+  total_time /= 35;
+  level_time /= 35;
+  
+  length += snprintf(
+    str,
+    max_size,
+    "%sM\n",
+    dsda_TextColor(dsda_tc_map_time_label)
+  );
+
+  if (local->always_show_both || total_time != level_time)
+    length += snprintf(
+      str + length,
+      max_size - length,
+      "%sT\n",
+      dsda_TextColor(dsda_tc_map_time_label)
+  );
+}
 
 static void dsda_UpdateComponentText(char* str, size_t max_size) {
   int length;
@@ -40,19 +70,7 @@ static void dsda_UpdateComponentText(char* str, size_t max_size) {
   total_time /= 35;
   level_time /= 35;
 
-  if (local->show_labels)
-    length = snprintf(
-      str,
-      max_size,
-      "%sM %s%02d:%02d:%02d\n",
-      dsda_TextColor(dsda_tc_map_time_level_label),
-      dsda_TextColor(dsda_tc_map_time_level),
-      level_time / 3600,
-      (level_time % 3600) / 60,
-      level_time % 60
-  );
-  else
-    length = snprintf(
+  length = snprintf(
       str,
       max_size,
       "%s%02d:%02d:%02d\n",
@@ -63,29 +81,15 @@ static void dsda_UpdateComponentText(char* str, size_t max_size) {
   );
 
   if (local->always_show_both || total_time != level_time)
-  {
-    if (local->show_labels)
-      snprintf(
-        str + length,
-        max_size - length,
-        "%sT %s%02d:%02d:%02d\n",
-        dsda_TextColor(dsda_tc_map_time_total_label),
-        dsda_TextColor(dsda_tc_map_time_total),
-        total_time / 3600,
-        (total_time % 3600) / 60,
-        total_time % 60
-    );
-    else
-      snprintf(
-        str + length,
-        max_size - length,
-        "%s%02d:%02d:%02d\n",
-        dsda_TextColor(dsda_tc_map_time_total),
-        total_time / 3600,
-        (total_time % 3600) / 60,
-        total_time % 60
-    );
-  }
+    snprintf(
+      str + length,
+      max_size - length,
+      "%s%02d:%02d:%02d\n",
+      dsda_TextColor(dsda_tc_map_time_total),
+      total_time / 3600,
+      (total_time % 3600) / 60,
+      total_time % 60
+  );
 }
 
 void dsda_InitMapTimeHC(int x_offset, int y_offset, int vpt, int* args, int arg_count, void** data) {
@@ -95,12 +99,15 @@ void dsda_InitMapTimeHC(int x_offset, int y_offset, int vpt, int* args, int arg_
   local->always_show_both = arg_count > 0 ? !!args[0] : false;
   local->show_labels = arg_count > 1 ? !!args[1] : false;
 
-  dsda_InitBlockyHC(&local->component, x_offset, y_offset, vpt);
+  dsda_InitBlockyHC(&local->label, x_offset, y_offset, vpt);
+  dsda_InitBlockyHC(&local->component, x_offset + (local->show_labels ? 12 : 0), y_offset, vpt);
 }
 
 void dsda_UpdateMapTimeHC(void* data) {
   local = data;
 
+  dsda_UpdateLabelComponentText(local->label.msg, sizeof(local->label.msg));
+  dsda_RefreshHudText(&local->label);
   dsda_UpdateComponentText(local->component.msg, sizeof(local->component.msg));
   dsda_RefreshHudText(&local->component);
 }
@@ -108,5 +115,8 @@ void dsda_UpdateMapTimeHC(void* data) {
 void dsda_DrawMapTimeHC(void* data) {
   local = data;
 
-  dsda_DrawBasicShadowedText(&local->component);
+  if (local->show_labels)
+    dsda_DrawBasicShadowedText(&local->label);
+  dsda_DrawBasicShadowedText(&local->component
+  );
 }
