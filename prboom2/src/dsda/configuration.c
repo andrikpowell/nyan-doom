6cb3b030058f4a254d2147326cae88153a3de9d5
Arsinikk <me@andrikpowell.com>
2026-08-18 21:43:53 -0500

    Set "Allow Multiple Map Pickups" default to false

--- prboom2/src/dsda/configuration.c
+++ prboom2/src/dsda/configuration.c
@@ -1758,7 +1758,7 @@ dsda_config_t dsda_config[dsda_config_count] = {
   },
   [dsda_config_multiple_area_maps] = {
     "dsda_multiple_area_maps", dsda_config_multiple_area_maps,
-    CONF_BOOL(1), NULL, STRICT_INT(0)
+    CONF_BOOL(0), NULL, STRICT_INT(0)
   },
   [dsda_config_doomguy_angry_face_fix] = {
     "dsda_doomguy_angry_face_fix", dsda_config_doomguy_angry_face_fix,
