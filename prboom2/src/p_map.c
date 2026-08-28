0af939a95a55e581d10b2d984e990bba934afde6
Arsinikk <me@andrikpowell.com>
2026-08-27 20:45:17 -0500

    Do pre-tic interpolation for all mobjs (Take II) - Fixes jitter for when an item dropped by an enemy on a moving lift does not interpolate z because the lift thinker happens before the item thinker - This time we approach this differently cuz last one did fix the lift jitter, but caused jitter for things moving up and down on floors.

--- prboom2/src/p_map.c
+++ prboom2/src/p_map.c
@@ -1973,6 +1973,8 @@ dboolean P_ThingHeightClip (mobj_t* thing)
 {
   dboolean   onfloor;
 
+  P_MobjInterpolation(thing);
+
   onfloor = (thing->z == thing->floorz);
 
   P_CheckPosition (thing, thing->x, thing->y);
