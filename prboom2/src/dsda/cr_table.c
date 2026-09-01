99bde567380e80ebb4ba2ba64285fa3565c4a25a
Arsinikk <me@andrikpowell.com>
2026-09-01 01:22:00 -0500

    Fix dark font colour translations - Fixes Heretic/Hexen muted colours

--- prboom2/src/dsda/cr_table.c
+++ prboom2/src/dsda/cr_table.c
@@ -105,7 +105,7 @@ static void dsda_CalculateFontBounds(const byte* playpal) {
 
       for (j = 0; j < length; ++j) {
         entry = *p++;
-        lightness = dsda_PaletteEntryLightness(playpal, entry) / 100.0;
+        lightness = dsda_PaletteEntryIntensity(playpal, entry);
         dsda_RegisterFontLightness(lightness);
       }
 
@@ -202,7 +202,7 @@ static void dsda_LoadCRLumps(byte* buffer) {
 static double dsda_GetCRBrightness(const byte *playpal, int index) {
   double length;
 
-  length = dsda_PaletteEntryLightness(playpal, index) / 100.0;
+  length = dsda_PaletteEntryIntensity(playpal, index);
   length -= cr_font.light_lower_bound;
   length *= cr_font.multiplier;
   if (length < 0)
