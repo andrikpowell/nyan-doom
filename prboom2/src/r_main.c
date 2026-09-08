a5c76e2d8fc517d934cf843eb71fed382722a74b
Arsinikk <me@andrikpowell.com>
2026-09-07 23:01:52 -0500

    Fix Linux CI warning

--- prboom2/src/r_main.c
+++ prboom2/src/r_main.c
@@ -995,7 +995,10 @@ static void R_ProcessFOV(void)
     fov_zoom_active = false;
 
   if (fov_zoom_active)
-    target_fov = (float)dsda_IntConfig(dsda_config_zoom_fov);
+  {
+    int zoom_fov = dsda_IntConfig(dsda_config_zoom_fov);
+    target_fov = (float)zoom_fov;
+  }
 
   if (base_fov != old_base_fov)
   {
