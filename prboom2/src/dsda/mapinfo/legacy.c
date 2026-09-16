kraflab/dsda-doom

Fix Episode 4 par times (#858)
Fixes #857

prboom2/src/dsda/mapinfo/legacy.c
@@ -552,8 +552,16 @@ void dsda_LegacyParTime(int* partime, dboolean* modified) {
     }
   }
   else {
-    if (gameepisode >= 1 && gameepisode <= 4 && gamemap >= 1 && gamemap <= 9) {
+    if (gameepisode >= 1 &&
+        (gameepisode <= 3 || (allow_incompatibility && gameepisode <= 4)) &&
+        gamemap >= 1 && gamemap <= 9) {
       *partime = TICRATE * pars[gameepisode][gamemap];
+      *modified = deh_pars;
+    }
+    // Doom episode 4 doesn't have a par time, so this
+    // overflows into the cpars array.
+    else if (gameepisode == 4 && gamemap >= 1 && gamemap <= 9) {
+      *partime = TICRATE * cpars[gamemap - 1];
       *modified = deh_pars;
     }
   }

andrikpowell/nyan-doom

prboom2/src/dsda/mapinfo/legacy.c
@@ -968,8 +968,16 @@ void dsda_LegacyParTime(int* partime, dboolean* modified) {
     }
   }
   else {
-    if (gameepisode >= 1 && gameepisode <= 4 && gamemap >= 1 && gamemap <= 9) {
+    if (gameepisode >= 1 &&
+        (gameepisode <= 3 || (casual_play && gameepisode <= 4)) &&
+        gamemap >= 1 && gamemap <= 9) {
       *partime = TICRATE * pars[gameepisode][gamemap];
+      *modified = deh_pars;
+    }
+    // Doom episode 4 doesn't have a par time, so this
+    // overflows into the cpars array.
+    else if (gameepisode == 4 && gamemap >= 1 && gamemap <= 9) {
+      *partime = TICRATE * cpars[gamemap - 1];
       *modified = deh_pars;
     }
   }